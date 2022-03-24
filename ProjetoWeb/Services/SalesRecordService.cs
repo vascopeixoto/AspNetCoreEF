87586f7dcfde2ba5c4071010edf7f2e23e3a6498
vascopeixoto <vascomfpeixoto@gmail.com>
2022-03-24 17:12:46 +0000

    versão final

--- ProjetoWeb/Services/SalesRecordService.cs
+++ ProjetoWeb/Services/SalesRecordService.cs
@@ -2,6 +2,8 @@
 using ProjetoWeb.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ProjetoWeb.Services
 {
@@ -14,9 +16,47 @@ namespace ProjetoWeb.Services
             _context = context;
         }
 
-        public List<SalesRecord> FindAll()
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
-            return _context.SalesRecord.ToList();
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x=>x.Seller)
+                .Include(x=>x.Seller.Department)
+                .OrderByDescending(x=>x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            var data = await result
+
+                .Include(s => s.Seller)
+
+                .Include(s => s.Seller.Department)
+
+                .OrderByDescending(s => s.Date)
+
+                .ToListAsync();
+
+            return data.GroupBy(s => s.Seller.Department).ToList();
         }
 
 
